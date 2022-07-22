359a4152670ea9ce5e31367b27abc569d49ae3ed
Pranay Dam <60703119+pranaydam@users.noreply.github.com>
2022-07-22 22:29:37 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -30,10 +30,7 @@ def main():
     st.image(image)
     st.markdown("# *What's Cooking? :cooking:*")
 
-    st.markdown(
-        "An ML powered app by Jack Leitch <a href='https://github.com/jackmleitch/whatscooking-deployment' > <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png' width='20' height='20' > </a> ",
-        unsafe_allow_html=True,
-    )
+   
     st.markdown(
         "## Given a list of ingredients, what different recipes can I can make? :tomato: "
     )
